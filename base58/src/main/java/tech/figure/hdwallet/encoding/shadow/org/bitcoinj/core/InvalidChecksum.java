34866d1d40ab730f111ed8128ae620d4ae736ce7
Phil Story <mtps@users.noreply.github.com>
2023-05-02 17:44:13 -0600

    Quiet down linter complaints.

--- base58/src/main/java/tech/figure/hdwallet/encoding/shadow/org/bitcoinj/core/InvalidChecksum.java
+++ base58/src/main/java/tech/figure/hdwallet/encoding/shadow/org/bitcoinj/core/InvalidChecksum.java
@@ -1,4 +1,7 @@
 package tech.figure.hdwallet.encoding.shadow.org.bitcoinj.core;
 
+/**
+ * InvalidChecksum exception type.
+ */
 public class InvalidChecksum extends Throwable {
 }
--- base58/src/main/java/tech/figure/hdwallet/encoding/shadow/org/bitcoinj/core/InvalidDataLength.java
+++ base58/src/main/java/tech/figure/hdwallet/encoding/shadow/org/bitcoinj/core/InvalidDataLength.java
@@ -1,6 +1,13 @@
 package tech.figure.hdwallet.encoding.shadow.org.bitcoinj.core;
 
+/**
+ * InvalidDataLength exception type.
+ */
 public class InvalidDataLength extends Throwable {
+    /**
+     * InvalidDataLength
+     * @param s the error message.
+     */
     public InvalidDataLength(String s) {
         super(s);
     }
